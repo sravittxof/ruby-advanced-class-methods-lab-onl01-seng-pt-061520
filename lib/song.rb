2e77ac4822d523d1bd3d9986f7f51cd38707ffcb
Travis Fox <travis.fox90@gmail.com>
2020-07-14 23:56:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,7 +57,7 @@ class Song
   def self.new_from_file(file_name)
     song = self.new
     
-    song.name = file_name.split("-").scan(/(\w+/)
+    song.name = file_name.split("-").scan(/(\w+).mp3/)
   end
   
 end
